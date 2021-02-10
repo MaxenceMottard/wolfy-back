013856a6c9206b696cd7d67bf8a5363b19093176
MaxenceMottard <mottard.maxence@gmail.com>
2021-02-10 22:16:27 +0100

    fix server to use socket.io and API

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
-/* eslint-disable no-console,@typescript-eslint/no-var-requires */
-import express, { Application } from 'express';
+/* eslint-disable no-console,@typescript-eslint/no-var-requires,import/no-extraneous-dependencies,global-require */
+import express, { Application, Response } from 'express';
+import fs from 'fs';
+import path from 'path';
 import sockets from './socket';
 
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const { PORT } = process.env;
+
 const app: Application = express();
-const server = app.listen(80, () => console.log('SERVER STARTED'));
-const io = require('socket.io')(server, { path: '/' });
+
+app.get('/apple-app-site-association', (_, res: Response) => {
+    fs.readFile(path.join(__dirname, '../apple-app-site-association.json'), (_, data) => {
+        res.contentType('application/json').send(data.toString());
+    });
+});
+
+const server = app.listen(PORT, () => console.log(`SERVER STARTED PORT=${PORT}`));
+const io = require('socket.io')(server, { path: '/socket.io' });
 
 sockets(io);
