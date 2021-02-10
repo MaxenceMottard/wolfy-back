7639061a2bb72a1e1c22e08d054abfd77959b94f
MaxenceMottard <mottard.maxence@gmail.com>
2021-02-10 13:15:10 +0100

    change port fort PROD

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express, { Application } from 'express';
 import sockets from './socket';
 
 const app: Application = express();
-const server = app.listen(9999, () => console.log('SERVER STARTED'));
+const server = app.listen(80, () => console.log('SERVER STARTED'));
 
 const io = require('socket.io')(server, { path: '/' });
 
